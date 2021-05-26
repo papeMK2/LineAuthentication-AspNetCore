b75d178994a8dc6faed66e91fccc85efec4e5a33
Takaaki Suzuki <xin9le@live.jp>
2021-05-26 15:31:52 +0900

    Prefer const

--- Identity.LineAccountExtension/LineAccountDefault.cs
+++ Identity.LineAccountExtension/LineAccountDefault.cs
@@ -3,9 +3,9 @@
     public static class LineAccountDefault
     {
         public const string AuthenticationScheme = "Line";
-        public static readonly string DisplayName = "Line";
-        public static readonly string AuthorizationEndpoint = "https://access.line.me/oauth2/v2.1/authorize";
-        public static readonly string TokenEndpoint = "https://api.line.me/oauth2/v2.1/token";
-        public static readonly string UserInformationEndpoint = "https://api.line.me/v2/profile";
+        public const string DisplayName = "Line";
+        public const string AuthorizationEndpoint = "https://access.line.me/oauth2/v2.1/authorize";
+        public const string TokenEndpoint = "https://api.line.me/oauth2/v2.1/token";
+        public const string UserInformationEndpoint = "https://api.line.me/v2/profile";
     }
 }
